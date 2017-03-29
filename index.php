556cb137afd9295b64f41a064ffce0beca0999ab
Daniel Pullan <dandandrumsgm@gmail.com>
2017-03-29 15:01:49 +0100

    filling out the stylesheet
    makes sense to do it now so that i can get it out of the way

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <html>
 <head>
     <title>Thorium</title>
-    <link rel="stylesheet" href="/css/style.css"
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
 </head>
 
 <body>
-<h1>Test</h1>
+<!-- Header part -->
+
+
+<!-- Body Part -->
+
+
+<!-- Footer Part -->
 
 
 </body>
