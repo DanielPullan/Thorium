3a77b36faeb6026f6dbacaa055fd29ccd6952570
Daniel Pullan <dandandrumsgm@gmail.com>
2017-03-30 10:48:08 +0100

    added basic slider for testing
    also adding bootstrap (not calling it) and some css fixes

--- index.php
+++ index.php
@@ -3,18 +3,46 @@
     <title>Thorium</title>
     <link rel="stylesheet" href="/css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+<!--    <link rel="stylesheet" href="/css/bootstrap.min.css" -->
 </head>
 
 <body>
 <!-- Header part -->
-
-
+<h1>Header</h1>
 <!-- Body Part -->
+<p>Libre ipsum something all work and no play makes homer something something</p>
 
 
+<div id="slideshow">
+   <div>
+     <img src="//farm6.static.flickr.com/5224/5658667829_2bb7d42a9c_m.jpg">
+   </div>
+   <div>
+     <img src="//farm6.static.flickr.com/5230/5638093881_a791e4f819_m.jpg">
+   </div>
+   <div>
+     Pretty cool eh? This slide is proof the content can be anything.
+   </div>
+</div>
 <!-- Footer Part -->
 
 
 </body>
+<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
+<script>$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+</script>
 
 </html>
