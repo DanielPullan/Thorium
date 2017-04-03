ba4bbe3385612d02628ed99ac8c2c50f7c6db380
Daniel Pullan <dandandrumsgm@gmail.com>
2017-04-03 10:07:39 +0100

    adding comments and readme
    waiting for the first pi to finish installed a DE

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Thorium
 (I wanted a name related to Norse Mythology whilst keeping with the naming conventions at work.)
 
+## Requirements
+Currently, you need a bunch of linux based clients, I'm using OG Raspberry Pi's.
+You'll need a browser, a webserver and rsync installed on all the clients.
+You'll then need a host linux server, again i'm using a pi. This will need a webserver, rsync then whatever i need for the admin stuff.
+Then you'll need to be able to login with SSH keys from the master server to all the clients.
+
+I will add more instructions to use once I've finished this thing.
+
 
 A replacement for PHS's current Digital Signage Solution
 
--- export.py
+++ export.py
@@ -1,14 +1,19 @@
+## import stuff, can't remember if OS is needed but time and subprocess definately is
 import time
 import os
 import subprocess
 
+## ask the user what they want to sync to, save as a variable
 userInput = raw_input("What would you like to sync?: ")
 
+## define all the clients and the commands to rsync to them
 gyllir = ["rsync", "-a", "/var/www/html", "pi@gyllir:/var/www/", "--delete"]
 sinir = ["rsync", "-a", "/var/www/html", "pi@sinir:/var/www/", "--delete"]
 gisl = ["rsync", "-a", "/var/www/html", "pi@gisl:/var/www/", "--delete"]
 gulltopp = ["rsync", "-a", "/var/www/html", "pi@gulltopp:/var/www/", "--delete"]
 
+## if userInput is all, rsync to every pi. have 5 sec's inbetween as a breather for network
+## add time for logging
 if userInput == ("all"):
         subprocess.call(gyllir)
         print("exported to gyllir at "+time.strftime("%H:%M:%S"))
@@ -22,22 +27,26 @@ if userInput == ("all"):
         subprocess.call(gulltopp)
         print("exported to gulltopp at "+time.strftime("%H:%M:%S"))
 
-
+## only one pi
+## add time for logging
 elif userInput == ("gyllir"):
         subprocess.call(gyllir)
         print("exported to gyllir "+time.strftime("%H:%M:%S"))
-
+## only one pi
+## add time for logging
 elif userInput == ("sinir"):
         subprocess.call(sinir)
         print("exported to sinir "+time.strftime("%H:%M:%S"))
-
+## only one pi
+## add time for logging
 elif userInput == ("gisl"):
         subprocess.call(gisl)
         print("exported to gisl "+time.strftime("%H:%M:%S"))
-
+## only one pi
+## add time for logging
 elif userInput == ("gulltopp"):
         subprocess.call(gulltopp)
         print("exported to gulltopp "+time.strftime("%H:%M:%S"))
-
+## print what was input, say it wasn't recognised
 else:
-        print ("Sorry, "+ userInput+" is not recognised").
+        print ("Sorry, "+ userInput+" is not recognised")
