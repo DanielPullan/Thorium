0686cb7855b10ba2b6323fd67aaf80e3b314051a
Daniel Pullan <dandandrumsgm@gmail.com>
2017-04-03 09:59:20 +0100

    added time for logging

--- export.py
+++ export.py
@@ -11,33 +11,33 @@ gulltopp = ["rsync", "-a", "/var/www/html", "pi@gulltopp:/var/www/", "--delete"]
 
 if userInput == ("all"):
         subprocess.call(gyllir)
-        print("exported to gyllir")
+        print("exported to gyllir at "+time.strftime("%H:%M:%S"))
         time.sleep(5)
         subprocess.call(sinir)
-        print("exported to sinir")
+        print("exported to sinir at "+time.strftime("%H:%M:%S"))
         time.sleep(5)
         subprocess.call(gisl)
-        print("exported to gisl")
+        print("exported to gisl at "+time.strftime("%H:%M:%S"))
         time.sleep(5)
         subprocess.call(gulltopp)
-        print("exported to gulltopp")
+        print("exported to gulltopp at "+time.strftime("%H:%M:%S"))
 
 
 elif userInput == ("gyllir"):
         subprocess.call(gyllir)
-        print("exported to gyllir")
+        print("exported to gyllir "+time.strftime("%H:%M:%S"))
 
 elif userInput == ("sinir"):
         subprocess.call(sinir)
-        print("exported to sinir")
+        print("exported to sinir "+time.strftime("%H:%M:%S"))
 
 elif userInput == ("gisl"):
         subprocess.call(gisl)
-        print("exported to gisl")
+        print("exported to gisl "+time.strftime("%H:%M:%S"))
 
 elif userInput == ("gulltopp"):
         subprocess.call(gulltopp)
-        print("exported to gulltopp")
+        print("exported to gulltopp "+time.strftime("%H:%M:%S"))
 
 else:
         print ("Sorry, "+ userInput+" is not recognised").
