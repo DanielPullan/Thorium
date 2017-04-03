aedc0dce84c7fd10642230027cd4c24526f9a5a2
Daniel Pullan <dandandrumsgm@gmail.com>
2017-04-03 11:00:57 +0100

    fleshing out the export script to something... bigger..

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
 # Thorium
+A replacement for PHS's current Digital Signage Solution
 (I wanted a name related to Norse Mythology whilst keeping with the naming conventions at work.)
 
 ## Requirements
 Currently, you need a bunch of linux based clients, I'm using OG Raspberry Pi's.
-You'll need a browser, a webserver and rsync installed on all the clients.
+You'll need a browser, a webserver and rsync installed on all the clients. The clients will need a desktop environment and xserver.
 You'll then need a host linux server, again i'm using a pi. This will need a webserver, rsync then whatever i need for the admin stuff.
 Then you'll need to be able to login with SSH keys from the master server to all the clients.
 
 I will add more instructions to use once I've finished this thing.
-
-
-A replacement for PHS's current Digital Signage Solution
 
 ### Versioning explained
 Major.Minor.Status
 Statuses release, rc (release candidate), beta, dev
 
 For example, 1.0.dev is the development of version 1.0. 1.0.release would be the public version.
+
+
+- install chromium browser
+- set web directory to /var/www/Thorium
+- install lite version of raspbian
+
+sudo apt-get install --no-install-recommends xserver-xorg
+sudo apt-get install --no-install-recommends xinit
+sudo apt-get install raspberrypi-ui-mods
+sudo apt-get install chromium-browser
+
+nano /home/pi/.config/lxsession/LXDE-pi/autostart
+
+@chromium-browser --incognito --kiosk http://localhost/  # load chromium after boot and point to the localhost webserver in full screen mode (add on the last line)
--- export.py
+++ export.py
@@ -5,26 +5,34 @@ import subprocess
 
 ## ask the user what they want to sync to, save as a variable
 userInput = raw_input("What would you like to sync?: ")
+userCommand = raw_input("what would you like to do?: ")
+clientsAffected = raw_input("to which clients?: ")
+
 
 ## define all the clients and the commands to rsync to them
-gyllir = ["rsync", "-a", "/var/www/html", "pi@gyllir:/var/www/", "--delete"]
-sinir = ["rsync", "-a", "/var/www/html", "pi@sinir:/var/www/", "--delete"]
-gisl = ["rsync", "-a", "/var/www/html", "pi@gisl:/var/www/", "--delete"]
-gulltopp = ["rsync", "-a", "/var/www/html", "pi@gulltopp:/var/www/", "--delete"]
+exportGyllir = ["rsync", "-a", "/var/www/html", "pi@gyllir:/var/www/", "--delete"]
+exportSinir = ["rsync", "-a", "/var/www/html", "pi@sinir:/var/www/", "--delete"]
+exportGisl = ["rsync", "-a", "/var/www/html", "pi@gisl:/var/www/", "--delete"]
+exportGulltopp = ["rsync", "-a", "/var/www/html", "pi@gulltopp:/var/www/", "--delete"]
+
+restartGyllir = ["SSH", "pi@gyllir", "sudo", "shutdown", "-r", "now"]
+resstartSinir = ["SSH", "pi@sinir", "sudo", "shutdown", "-r", "now"]
+restartGisl = ["SSH", "pi@gisl", "sudo", "shutdown", "-r", "now"]
+restartGulltopp = ["SSH", "pi@gulltopp", "sudo", "shutdown", "-r", "now"]
 
 ## if userInput is all, rsync to every pi. have 5 sec's inbetween as a breather for network
 ## add time for logging
 if userInput == ("all"):
-        subprocess.call(gyllir)
+        subprocess.call(exportGyllir)
         print("exported to gyllir at "+time.strftime("%H:%M:%S"))
         time.sleep(5)
-        subprocess.call(sinir)
+        subprocess.call(exportSinir)
         print("exported to sinir at "+time.strftime("%H:%M:%S"))
         time.sleep(5)
-        subprocess.call(gisl)
+        subprocess.call(exportGisl)
         print("exported to gisl at "+time.strftime("%H:%M:%S"))
         time.sleep(5)
-        subprocess.call(gulltopp)
+        subprocess.call(exportGulltopp)
         print("exported to gulltopp at "+time.strftime("%H:%M:%S"))
 
 ## only one pi
