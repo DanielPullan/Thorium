b03f9ed5f010e9e1942d4424e067b5538f1f289f
Daniel Pullan <dandandrumsgm@gmail.com>
2017-05-05 09:45:59 +0100

    working state
    i am now going to do this properly. i shouldn't be doing things this way

--- admin/insert.php
+++ admin/insert.php
@@ -1,6 +1,18 @@
 <?php
 
-require '/core/mysql.class.php';
+function Connect()
+{
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "1123";
+ $dbname = "thorium";
+
+ // Create connection
+ $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
+
+ return $conn;
+}
+
 $conn    = Connect();
 $date    = $_POST['date'];
 $title   = $_POST['title'];
@@ -9,12 +21,12 @@ $query   = "INSERT INTO calendar (date, title, description) VALUES ('$date','$ti
 $success = $conn->query($query);
 
 if (!$success) {
-    die($conn->error);
+    die("Couldn't enter data: ".$conn->error);
 
 }
 
 $conn->close();
 
-echo "Event added, click <a href='http://localhost'>here</a> to see it";
+echo 'Event added, click <a href="http://localhost">here</a> to see it '
 
 ?>
