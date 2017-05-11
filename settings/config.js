7831ec52e3fb98298c05e550bcfc1995789e8400
Daniel Pullan <dandandrumsgm@gmail.com>
2017-05-11 11:33:58 +0100

    and the one that got left behind :crocodile:

---
+++ settings/config.js
@@ -0,0 +1,16 @@
+var url = "localhost"
+var name = "Poole High";
+var twitter = "poole_high";
+var facebook = "poolehs";
+var twapi = "https://twitter.com/poole_high";
+var bannerText  = "Look I won the no-bell prize";
+//back burner for now
+//var bannerIcon = "fa fa-bell-slash";
+
+document.getElementById('name').innerHTML = name;
+document.getElementById('twitter').innerHTML = twitter;
+document.getElementById('facebook').innerHTML = facebook;
+document.getElementById('bannerText').innerHTML = bannerText;
+// backburner for now
+//document.getElementById('bannerIcon').innerHTML = bannerIcon;
+document.getElementById('twapi').innerHTML = twapi;
