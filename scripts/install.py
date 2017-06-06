538a8b632df8ca94dfe4fa262acc8a89f6b8423a
Daniel Pullan <dandandrumsgm@gmail.com>
2017-06-06 10:19:20 +0100

    can now set hostname easily
    to set a hostname of a pi, in /boot/ (which is accessible from Windows!)
    make a file called "hostname.txt" and in it, just write what you want to
    call the pi. do not use any special characters because i haven't tested
    what happens

--- scripts/install.py
+++ scripts/install.py
@@ -3,33 +3,45 @@ import os
 import subprocess
 import sys
 
+# if boot is zero, we haven't ran through the script yet
+if boot = 0:
+    # Enable SSH on First Boot
+    subprocess.call("sudo touch /boot/ssh")
+    # Find out the hostname
+    hostname = os.uname()
+    # If there is raspberry in the hostname, it's not been named yet so
+    if "raspberry" in hostname:
+        # Read the hostname file we created in boot
+        linestring = open('/boot/hostname.txt', 'r').read()
+        # Set it as new hostname
+        linestring = newHostname
+        # Then actually make it the new hostname
+        subprocess.call(["sudo", "hostnamectl", "set-hostname", newHostname])
+        # If there isn't raspberry in name, it has been named
+    else:
+        # Print this for logging
+        print(["This raspberry pi is called", hostname])
+    # first boot to 1, so we can count where we are in the process
+    firstBoot = 1
+        # Expand root filesystem, this requires a restart
+        subprocess.call(["sudo", "raspi-config","--expand-rootfs"])
+# if boot is 1, we've done all the first setup steps, so we have room to install
+# packages
+elif boot = 1:
+    # Update packages
+    print ("updating")
+    os.system("sudo apt-get update -y")
+    # Upgrade Packages
+    print ("upgrading")
+    os.system("sudo apt-get upgrade -y")
+    # Install PHP and PHP Related stuff
+    print ("installing php5 and php5-mysql")
+    os.system("sudo apt-get install php5 php5-mysql -y")
 
-firstBoot = raw_input("Is this the first boot? :")
+    # Install git and rsync
+    print ("installing git")
+    os.system("sudo apt-get install git rsync -y")
 
-if "yes" in firstBoot:
-    print ("expanding filesystem")
-    sudo raspi-config --expand-rootfs
-elif "no" in firstBoot:
-    print ("carry on my wayward son")
-else:
-    print (["Your response of ", firstBoot, " was not recognised, restart the script"])
-
-print ("updating")
-os.system("sudo apt-get update -y")
-
-print ("upgrading")
-os.system("sudo apt-get upgrade -y")
-
-print ("installing php5 and php5-mysql")
-os.system("sudo apt-get install php5 php5-mysql -y")
-
-print ("installing git")
-os.system("sudo apt-get install git rsync -y")
-
-hostname = os.uname()
-
-if "raspberry" in hostname:
-    newHostname = raw_input("What to call this pi?: ")
-    subprocess.call(["sudo", "hostnamectl", "set-hostname", newHostname])
-else:
-    print(["This raspberry pi is called", hostname])
+    # Install Apache2
+    print ("installing webserver")
+    os.system("sudo apt-get install apache2 -y")
