5ed8dbe83c3027ba73ce94a62f7777562839263a
Daniel Pullan <dandandrumsgm@gmail.com>
2017-06-27 10:19:00 +0100

    Added comments to export.py

--- scripts/export.py
+++ scripts/export.py
@@ -12,8 +12,13 @@ clientsAffected = sys.argv[1]
 # Make a thing
 exportToDevice = ["rsync", "-a", "/var/www/", "pi@"+clientsAffected, "--delete"]
 
+# Always run this script as true always return as true
 if True:
+    # Do a subprocess call that uses the array we set with all the information required for the rsync command
     subprocess.call(exportToDevice)
+    # Print that it was successful, useful for logging
     print("Exported to " + clientsAffected + " at " + time.strftime("%H:%N:%S"))
+# If true doesn't return as true, we have bigger fish to fry
 else:
+    # Print error, look into what loopy planet we're currently existing on.
     print("Error")
