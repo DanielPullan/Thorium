5aa30ea033614a9a24262a4a0a8ad9dfe6e4adfc
Daniel Pullan <dandandrumsgm@gmail.com>
2017-11-15 09:20:49 +0000

    wipe device list

---
+++ scripts/devices.py
@@ -0,0 +1,11 @@
+piCanteenWEST 	= "nada"
+piCanteenEAST 	= "nada"
+piMusic 	= "nada"
+piArt 		= "nada"
+piLester 	= "nada"
+piSixthform 	= "nada"
+piReception 	= "nada"
+piTheatre 	= "nada"
+piTesting 	= "nada"
+
+pi = [piCanteenWEST, piCanteenEAST, piMusic, piArt, piLester, piSixthform, piReception, piTheatre, piTesting]
