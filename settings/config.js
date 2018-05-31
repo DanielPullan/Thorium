319fb8e41caad0bc8e3367a1e49e4f4dfd3e1e35
Daniel Pullan <dandandrumsgm@gmail.com>
2018-05-31 09:43:35 +0100

    SchoolName can now be defined
    Also changed everything to Thorium names, this will be changable in the admin panel later.

--- settings/config.js
+++ settings/config.js
@@ -1,7 +1,9 @@
-var name = "Poole High";
-var twitter = "poole_high";
-var facebook = "poolehs";
+var name = "Thorium";
+var twitter = "ThoriumOnTwitter";
+var facebook = "ThoriumOnFacebook";
+var schoolName = "Thorium Academy";
 
 document.getElementById('name').innerHTML = name;
 document.getElementById('twitter').innerHTML = twitter;
 document.getElementById('facebook').innerHTML = facebook;
+document.getElementById('schoolName').innerHTML = schoolName;
